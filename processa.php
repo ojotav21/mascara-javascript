56040503b3870ed3bdf6cc19c7a85071ed02f1f2
João Victor Morais <60045344+ojotav21@users.noreply.github.com>
2020-07-09 19:40:58 -0300

    MascaraJS

---
+++ processa.php
@@ -0,0 +1,15 @@
+<?php
+
+$celular = $_POST['celular'];
+$rg = $_POST['rg'];
+$cpf = $_POST['cpf'];
+$telefone = $_POST['telefone'];
+$email = $_POST['email'];
+
+echo " <br> seu cpf :", $cpf;
+echo "<br> seu rg :", $rg;
+echo "<br> seu celular :", $celular;
+echo "<br> seu telefone :", $telefone;
+echo "<br> seu email :", $email;
+
+?>
